e28e6695fb0126773a28f3782a73c73f95f92061
dankozsolti <43618376+dankozsolti@users.noreply.github.com>
2019-03-17 15:59:22 +0100

    Create decimal.c

---
+++ decimal.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+  int decimal;
+  scanf ("%d", &decimal);
+  printf("Unáris:\n");
+
+for(int i=0; i < decimal; i++)
+  printf("1");
+  printf("\n");
+  return 0;
+}
